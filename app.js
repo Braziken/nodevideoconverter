7a1bb0eec6f4d2bca7d58a1a44afe46894b8b2f5
wemerson.silva <wemerson.silva@uaiti.com.br>
2013-05-24 16:09:58 -0300

    First commit, Life is good!

---
+++ app.js
@@ -0,0 +1,50 @@
+/**
+ * Module dependencies
+ */
+
+ var express = require('express');
+ var fs = require('fs');
+
+ var app = module.exports = express.createServer();
+
+// carrega as rotas
+var index = require('./routes/index').index;
+
+app.configure(function()
+{
+	app.use(express.bodyParser());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function()
+{
+	app.use(express.errorHandler({
+		dumpExceptions : true,
+		showStack : true
+	}));
+});
+
+app.configure('production', function()
+{
+	app.use(express.errorHandler());
+});
+
+// Routes
+
+function showIndex(req, res)
+{
+	fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text)
+	{
+		res.send(text);
+	});
+};
+
+app.get('/', showIndex);
+app.get('/index.html', showIndex);
+
+//app.get('/course/last', course.last);
+app.listen(3000, function()
+{
+	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
